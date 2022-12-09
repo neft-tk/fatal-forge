a88452117a543548c7c57906b0c64d8e6c038c2c
Jonathan Cornet <cornetj2@gmail.com>
2022-12-09 15:26:54 -0800

    Set up user login functionality!

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,7 +1,8 @@
-import blacksmith from '../../assets/blacksmith_nobg.gif'
+import blacksmithGif from '../../assets/gif/blacksmith_nobg.gif';
+import visibilityIcon from '../../assets/svg/visibilityIcon.svg';
 // The Login component provides a form and handler for users logging in.
 
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 // import API from "./utils/API.js";
 
 /*
@@ -11,59 +12,100 @@ i passed the setlogin state function through the props from the app component..
 that's the only change i made.
 */
 
-export default function Login(props) {
-  const [userId, setUserId] = useState(0)
-  const [token, setToken] = useState("")
-  const [loginEmail, setLoginEmail] = useState("")
-  const [loginPassword, setLoginPassword] = useState("")
+export default function Login({setIsLoggedIn, handleLogin}) {
+  const [userId, setUserId] = useState(0);
+  const [token, setToken] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [loginEmail, setLoginEmail] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
 
-  useEffect(() => {
-    // const storedToken = localStorage.getItem("token")
-    // if(storedToken){
-    //   console.log(storedToken)
-    //   API.getUserFromToken(storedToken).then(data=>{
-    //     if(data.user){
-    //       console.log(data)
-    //       setToken(storedToken)
-    //       props.setIsLoggedIn(true)
-    //       setUserId(data.user.id)
-    //     }
-    //   })
-    // } else {
-    //   console.log('no stored token')
-    // }
-  }, [])
+  // useEffect(() => {
+  //   // const storedToken = localStorage.getItem("token")
+  //   // if(storedToken){
+  //   //   console.log(storedToken)
+  //   //   API.getUserFromToken(storedToken).then(data=>{
+  //   //     if(data.user){
+  //   //       console.log(data)
+  //   //       setToken(storedToken)
+  //   //       props.setIsLoggedIn(true)
+  //   //       setUserId(data.user.id)
+  //   //     }
+  //   //   })
+  //   // } else {
+  //   //   console.log('no stored token')
+  //   // }
+  // }, []);
 
-  const handleLoginSubmit = e => {
+  const onEmailChange = (e) => {
+    setLoginEmail(e.target.value);
+  };
+
+  const onPasswordChange = (e) => {
+    setLoginPassword(e.target.value);
+  };
+
+  const handleLoginSubmit = (e) => {
     e.preventDefault();
-    // API.login({
-    //   email:loginEmail,
-    //   password:loginPassword
-    // }).then(data=>{
-    //   console.log(data);
-    //   if(data.token){
-    //     setUserId(data.user.id)
-    //     setToken(data.token)
-    props.setIsLoggedIn(true)
-    //     localStorage.setItem("token",data.token)
-    //   }
-    // })
-  }
-  const handleLogout = () => {
-    // localStorage.removeItem("token");
-    props.setIsLoggedIn(false);
-    // setUserId(0);
-    // setToken("");
-  }
+    // console.log("Button has been clicked!");
+    handleLogin({
+      email: loginEmail,
+      password: loginPassword
+    });
+  };
+
+  // const handleLogout = () => {
+  //   // localStorage.removeItem("token");
+  //   setIsLoggedIn(false);
+  //   // setUserId(0);
+  //   // setToken("");
+  // };
+
   return (
-    <div>
-    <form onSubmit={handleLoginSubmit}>
-      <h3>Login</h3>
-      <input name="email" value={loginEmail} onChange={e => setLoginEmail(e.target.value)} />
-      <input type="password" name="password" value={loginPassword} onChange={e => setLoginPassword(e.target.value)} />
-      <button>Log in!</button>
-    </form>
-    <img src={blacksmith} alt="A blacksmith hard at work." />
+    <div className="flex sm:flex-row flex-col">
+      <div className="flex justify-center items-center text-center sm:w-1/4 w-full">
+        <form onSubmit={handleLoginSubmit} className="flex flex-col">
+
+          <h3 className="text-cyan-400">Login</h3>
+
+          <input
+            name="email"
+            value={loginEmail}
+            placeholder="email"
+            onChange={onEmailChange}
+            className="text-black p-2"
+          />
+
+          <div className="relative m-0">
+            <input
+              type={showPassword ? 'text' : 'password'}
+              name="password"
+              value={loginPassword}
+              placeholder="password"
+              onChange={onPasswordChange}
+              className="text-black p-2"
+            />
+
+            <img
+              src={visibilityIcon}
+              alt="show password"
+              className="cursor-pointer absolute p-4 -top-2 -right-2"
+              onClick={() => setShowPassword((prevState) => !prevState)}
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="text-black bg-cyan-400 hover:bg-cyan-500 focus:ring-4 focus:ring-cyan-100 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-cyan-500 dark:hover:bg-cyan-700 focus:outline-none dark:focus:ring-cyan-300"
+          >
+            Login
+          </button>
+
+        </form>
+      </div>
+
+      <div className="flex p-4 sm:w-3/4 w-full justify-center items-center">
+        <img src={blacksmithGif} alt="A blacksmith hard at work." />
+      </div>
     </div>
-  )
+  );
 }
