0e45c85ffbb86a3883ed51c16b26e27f009c247c
victor <vicdotexe@gmail.com>
2022-12-08 22:38:04 -0800

    used new routes to validate room name and apply a state change to the gameview to render the initialize component

--- src/components/gameview/Gameview.jsx
+++ src/components/gameview/Gameview.jsx
@@ -2,12 +2,26 @@
 
 // The general flow of the Gameview will be: assembly => initialize => game
 
-import React from 'react'
+import React, { useState } from 'react'
 import Assembly from './assembly/Assembly'
+import Initialize from './initialize/Initialize';
+import Game from './game/Game'
 
 export default function Gameview() {
+  const [view, setView] = useState('assembly');
+  const [gameId, setGameId] = useState();
+  
+  function renderView(){
+    switch (view){
+      case 'assembly':
+        return <Assembly setView={setView} setGameId={setGameId}/>
+      case 'initialize':
+        return <Initialize gameId={gameId}/>
+      case 'game':
+        return <Game/>
+    }
+  }
   return (
-    // <div>Gameview</div>
-    <Assembly />
+    renderView()
   )
 }
