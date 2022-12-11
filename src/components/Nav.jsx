3c2d90f4cba27a0b6a51e3662b0dfcddb1401431
Jonathan Cornet <cornetj2@gmail.com>
2022-12-11 13:18:36 -0800

    Added a sidebar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,21 +4,24 @@
 
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { FaHome } from 'react-icons/fa'
-import HomeIcon from '.././assets/svg/HomeIcon'
+import { FaGamepad, FaUserFriends } from 'react-icons/fa'
+import { BsFillChatDotsFill } from 'react-icons/bs'
+import { GiCardAceSpades } from 'react-icons/gi'
+import { CgProfile } from 'react-icons/cg'
+import HomeIcon from '.././assets/svg/homeIcon.svg'
 import NavBarIcon from './NavBarIcon'
 
 export default function Nav(props) {
   return (
-    <div className='p-4 w-40 flex flex-col bg-zinc-900'>
+    <div className='p-2 w-28 flex flex-col bg-zinc-900 justify-evenly'>
         {/* <Link to="/gameview">Play</Link>
         <Link to="/deckbuilder">Decks</Link>
         <Link to="/lobby">Friends</Link> */}
-        <Link to="/lobby"><NavBarIcon icon={<FaHome size="40" />} text='Home' /></Link>
-        <Link to="/gameview"><NavBarIcon icon={<FaHome size="40" />} text='Game'/></Link>
-        <Link to="/deckbuilder"><NavBarIcon icon={<FaHome size="40" />} text='Deck Builder'/></Link>
-        <NavBarIcon icon={<FaHome size="40" />} text='Profile'/>
-        <NavBarIcon icon={<FaHome size="40" />} text='Friends'/>
+        <Link to="/gameview"><NavBarIcon icon={<FaGamepad size="40" />} text='Game'/></Link>
+        <Link to="/lobby"><NavBarIcon icon={<BsFillChatDotsFill size="40" />} text='Chat' /></Link>
+        <Link to="/deckbuilder"><NavBarIcon icon={<GiCardAceSpades size="40" />} text='Deck Builder'/></Link>
+        <Link to="/profile"><NavBarIcon icon={<CgProfile size="40" />} text='Profile'/></Link>
+        <Link to="/friends"><NavBarIcon icon={<FaUserFriends size="40" />} text='Friends'/></Link>
         {/* <NavBarIcon icon={<FaHome size="40" />} text='Stats'/> */}
         {/* <NavBarIcon icon={<FaHome size="40" />} text='Channels'/> */}
         {/* <NavBarIcon icon={<FaHome size="40" />} text='Settings'/> */}
