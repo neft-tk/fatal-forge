55c4d3a28e9ce3a2e4c64ea8a9c4dae5da5ba59c
Jonathan Cornet <cornetj2@gmail.com>
2022-12-14 10:52:23 -0800

    Cleaned up friends list and profile

--- src/components/pages/profile/PastGameCard.jsx
+++ src/components/pages/profile/PastGameCard.jsx
@@ -4,20 +4,20 @@ function PastGameCard({ deck }) {
   return (
     <>
       {/* TODO: Only model first card, map over victory/defeats from user. */}
-      <div className="flex flex-col m-6 items-center border-2 border-red-500 justify-around rounded bg-main-bg">
-        <h3 className="text-red-500 text-2xl font-bold mb-4">Defeat...</h3>
+      <div className="bg-main-bg border-red-600 flex flex-col m-6 items-center border-2 justify-around rounded">
+        <h3 className="text-red-600 text-2xl font-bold mb-4">Defeat...</h3>
         <div className="flex justify-around items-center w-full">
           <div className="flex flex-col items-center">
             <h3 className="text-main-text text-lg mb-2">Deck Used: </h3>
             <img
-              className="w-16 h-16 mb-2 border-2 border-black rounded-lg"
+              className="border-black w-16 h-16 mb-2 border-2  rounded-lg"
               src={`${Static.serverUrl}/api/images/${deck[0].imagePath}`}
               alt="Deck's Back of Card"
             />
             <h3 className="text-main-text text-lg">{deck[0].deckName}</h3>
           </div>
           <div>
-            <h3 className="text-center text-2xl font-bold text-main-text">
+            <h3 className="text-main-text text-center text-2xl font-bold">
               VS.
             </h3>
           </div>
@@ -33,8 +33,8 @@ function PastGameCard({ deck }) {
       </div>
 
       {/* TODO: Delete these placeholders when real data comes in. */}
-      <div className="flex flex-col m-6 items-center border-2 border-green-900 justify-around rounded bg-main-bg">
-        <h3 className="text-green-900 text-2xl font-bold mb-4">Victory!</h3>
+      <div className="bg-main-bg border-lime-600 flex flex-col m-6 items-center border-2 justify-around rounded">
+        <h3 className="text-lime-600 text-2xl font-bold mb-4">Victory!</h3>
         <div className="flex justify-around items-center w-full">
           <div className="flex flex-col items-center">
             <h3 className="text-main-text text-lg mb-2">Deck Used: </h3>
@@ -46,7 +46,7 @@ function PastGameCard({ deck }) {
             <h3 className="text-main-text text-lg">{deck[0].deckName}</h3>
           </div>
           <div>
-            <h3 className="text-center text-2xl font-bold text-main-text">
+            <h3 className="text-main-text text-center text-2xl font-bold">
               VS.
             </h3>
           </div>
