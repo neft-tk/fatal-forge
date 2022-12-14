1d49fcd0def599a8a43e923255e1da8b049f1fdb
joerdixon <joseph35dixon@gmail.com>
2022-12-14 10:05:57 -0800

    home layout 50%

--- src/components/pages/lobby/Chat.jsx
+++ src/components/pages/lobby/Chat.jsx
@@ -27,8 +27,8 @@ export default function Chat({ userId }) {
   }, []);
 
   return (
-    <div className='h-3/4 flex flex-col justify-evenly align-middle items-center'>
-      <div className='bg-alt-bg w-2/5 h-2/5'>
+    <div className='h-full flex flex-col justify-evenly align-middle items-center'>
+      <div className='bg-alt-bg w-full h-full min-w-1/2 min-h-1/2'>
       </div>
       <form className='flex flex-col justify-evenly h-1/4 items-center w-2/5' onSubmit={handleFormSubmit}>
         <label htmlFor="message-input" className=''>Chat:</label>
--- src/components/pages/lobby/Lobby.jsx
+++ src/components/pages/lobby/Lobby.jsx
@@ -5,10 +5,35 @@
 import React from 'react'
 import Chat from './Chat'
 import gllogo from '../../../assets/png/gridlocke1.png'
-
 export default function Lobby({ userId }) {
 
   return (
-    <Chat userId={userId} />
+    <div className='flex flex-col justify-evenly items-center h-screen'>
+      {/* Hero */}
+      <img src={gllogo} alt="Gridlocke logo" className='w-1/4 mx-auto my-0' />
+      {/* Bio */}
+      <p className='h-1/4 w-full mx-auto my-0 font-main-text-f text-center'>
+        <span className='font-alt-text-f'>Welcome to <span className=' text-3xl font-bold text-highlight-orange'>GridLocke</span>, the deck-building tactical card game!</span>
+        <br />
+        <br />
+        Fulfill your role as a destined decksmith and wield magically infused cards to battle with friends online!
+        <br />
+        <br />
+        Build your deck with diverse, unique, and fun strategies in order to take majority control of the grid!
+        <br />
+        <br />
+        Meet on a battlefield of 3x3, 4x4, or 5x5 grid slots to test different aspects of your aptitude for battle.
+      </p>
+      {/* Chat + Active Players */}
+      {/* <div className='h-screen'>
+        <Chat userId={userId} />
+      </div> */}
+      {/* Chat */}
+      {/* Future Updates */}
+      {/* <div className='h-1/3'>
+          Future Updates:
+        </div> */}
+
+    </div>
   )
 }
