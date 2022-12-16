3a8fe2f07f6cabf74ac2043e18543d07afa36d36
victor <vicdotexe@gmail.com>
2022-12-16 10:31:40 -0800

    implemented mobile

--- src/components/gameview/Gameview.jsx
+++ src/components/gameview/Gameview.jsx
@@ -6,9 +6,27 @@ import React, { useEffect, useState } from 'react'
 import Assembly from './Assembly'
 import Initialize from './Initialize';
 import Game from './game/Game'
-import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend'
+import { TouchBackend } from 'react-dnd-touch-backend'
+import { DndProvider, TouchTransition, MouseTransition, Preview } from 'react-dnd-multi-backend'
 import Socket from '../../utils/socket';
+
+export const HTML5toTouch = {
+  backends: [
+    {
+      id: 'html5',
+      backend: HTML5Backend,
+      transition: MouseTransition,
+    },
+    {
+      id: 'touch',
+      backend: TouchBackend,
+      options: {enableMouseEvents: true},
+      preview: true,
+      transition: TouchTransition,
+    },
+  ],
+}
 
 export default function Gameview() {
   const [view, setView] = useState('assembly');
@@ -23,6 +41,16 @@ export default function Gameview() {
     }
   },[])
 
+  const generatePreview = ({itemType, item, style}) => {
+    if (itemType=='card' && Socket.IO.preview){
+      return <div style={{position:'fixed', 
+      top:Socket.IO.position.y -50, left:Socket.IO.position.x -50,
+      backgroundColor:'yellow', pointerEvents:'none'}}>{Socket.IO.preview}</div>
+    }else{
+      return null
+    }
+    
+  }
   
   function renderView(){
     switch (view){
@@ -33,7 +61,8 @@ export default function Gameview() {
       case 'game':
         return (
 
-            <DndProvider backend={HTML5Backend}>
+            <DndProvider options={HTML5toTouch}>
+              <Preview generator={generatePreview} />
               <Game deckId={deck} size={size} gameId={gameId} setView={setView}/>
             </DndProvider>
         )
